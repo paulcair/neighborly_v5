0aad6254c894ce79813b4b0460b46c9472005c83
Raphael Goemans <raphaelgoemans@Raphaels-MacBook-Air.local>
2023-04-29 23:06:08 -0400

    Added real tasks

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,13 @@ export default function Home() {
   const contractAddress = "0xB2c8A7B59628D8c6c50cD88BC0c9b4Ca800387f9";
 
   const { contract } = useContract(contractAddress);
-  const { data: task, isLoading: taskLoading } = useContractRead(contract, "getTask", [0]);
-  const { data: taskCount, isLoading: taskCountLoading } = useContractRead(contract, "getTaskCount");
-
 
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-welcome">
         < Navbar />
         < Welcome />
-        < CreateTask />
+        < CreateTask contract={contract} />
         < Tasks />
         < Footer />
       </div>      
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,41 +1,108 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useContract, useContractRead } from "@thirdweb-dev/react";
+
+const contractAddress = "0xB2c8A7B59628D8c6c50cD88BC0c9b4Ca800387f9";
 
 const TaskCard = ({ name, details, imageURL }) => (
-         <div className="w-full lg:flex m-4">
-            <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style={{backgroundImage: `url(${imageURL})`}}
-            >
-            </div>
-         <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-            <div className="w-full mb-8">
-                <div className="text-gray-900 font-bold text-xl mb-2">{name}</div>
-                <p className="text-gray-700 text-base">{details}</p>
-                </div>
-                <button 
-                    type="button"
-                    className="text-white w-full mt-2 border-[1px] p-2 border-[#A08850] bg-[#A08850] hover:bg-[#7C5B2E] hover:border-[#7C5B2E] rounded-full cursor-pointer"
-                >
-                Add Tip
-                </button>
-            </div>
+    <div className="w-full lg:flex m-4">
+      <div
+        className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t-lg lg:rounded-t-none lg:rounded-l-lg text-center overflow-hidden"
+        style={{ backgroundImage: `url(${imageURL})` }}
+      ></div>
+      <div className="w-full lg:w-130 border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b-lg lg:rounded-b-none lg:rounded-r-lg p-4 flex flex-col justify-between leading-normal">
+        <div className="w-full mb-8">
+          <div className="text-gray-900 font-bold text-xl mb-2">{name}</div>
+          <p className="text-gray-700 text-base">{details}</p>
         </div>
-);
-
-
-const Tasks = () => (
-    <div className="flex w-full justify-center items-center">
-           <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
-            <TaskCard
-                name = "Test"
-                details = "test text details lets make this longer and longer and longer and longer and longer and longer"
-                imageURL = "https://www.cityworks.com/wp-content/uploads/2020/01/pot-hole-blog.gif"
-            />
-            <TaskCard
-                name = "Test"
-                details = "test text details let's make this one longer and longer and longer and longer and longer and longer"
-                imageURL = "https://www.cityworks.com/wp-content/uploads/2020/01/pot-hole-blog.gif"
-            />
+        <div className="flex space-x-2">
+          <button
+            type="button"
+            className="text-white w-28 p-2 bg-green-500 hover:bg-green-700 border border-green-500 hover:border-green-700 rounded-lg cursor-pointer"
+          >
+            Add Tip
+          </button>
+          <button
+            type="button"
+            className="text-white w-56 p-2 bg-gray-500 hover:bg-gray-700 border border-gray-500 hover:border-gray-700 rounded-lg cursor-pointer"
+          >
+            Upload Proof of Completion
+          </button>
         </div>
+      </div>
     </div>
-);
+  );
+  
+  
+
+const Tasks = (props) => {
+  const { contract } = useContract(contractAddress);
+  const { data: taskCount, isLoading: taskCountLoading } = useContractRead(contract, "getTaskCount");
+  
+  const { data: task3, isLoading: task3Loading } = useContractRead(contract, "getTask", [3]);
+  const { data: task2, isLoading: task2Loading } = useContractRead(contract, "getTask", [2]);
+  const { data: task4, isLoading: task4Loading } = useContractRead(contract, "getTask", [4]);
+//   const { data: task3, isLoading: task3Loading } = useContractRead(contract, "getTask", [3]);
+
+
+  
+//   const [tasks, setTasks] = useState([]);
+
+//   const fetchTask = async (index) => {
+//     const { data: task } = await useContractRead(contract, "getTask", [index]);
+//     return task;
+//   };
+
+//   useEffect(() => {
+//     if (!taskCountLoading && taskCount) {
+//       const fetchTasks = async () => {
+//         const taskPromises = Array.from({ length: taskCount }, (_, i) => fetchTask(i));
+//         const fetchedTasks = await Promise.all(taskPromises);
+//         setTasks(fetchedTasks);
+//       };
+
+//       fetchTasks();
+//     }
+//   }, [taskCount, taskCountLoading, contract]);
+
+
+
+
+//   const renderTaskCards = () => {
+//     return (
+//       <TaskCard
+//         name={task0[0]}
+//         details={task0[1]}
+//         imageURL={task0[2]}
+//       />
+//     );
+//   };
+
+  return (
+    <div className="flex w-full justify-center items-center">
+      <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+        {taskCountLoading ? <p>Loading...</p> : 
+        <div>
+            <TaskCard
+        name={task3[0]}
+        details={task3[1]}
+        imageURL={task3[2]}
+      />
+      <TaskCard
+        name={task2[0]}
+        details={task2[1]}
+        imageURL={task2[2]}
+      />
+      <TaskCard
+        name={task4[0]}
+        details={task4[1]}
+        imageURL={task4[2]}
+      />
+      </div>
+
+        }
+      </div>
+    </div>
+  );
+};
 
 export default Tasks;
